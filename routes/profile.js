5d8a74c6ed35ab8de1a8cab8dfaafcdd6e266883
Edouard-Pon <91964969+Edouard-Pon@users.noreply.github.com>
2023-04-17 03:36:58 +0200

    Added profile router

---
+++ routes/profile.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+const Profile = require('../models/profile')
+
+router.get('/', async (req, res) => {
+    try {
+        const profile = await Profile.find().exec()
+        res.render('profile/index', { profile: profile })
+    } catch {
+        res.redirect('/')
+    }
+})
+
+router.get('/new', (req, res) => {
+    res.render('profile/new', { profile: new Profile() })
+})
