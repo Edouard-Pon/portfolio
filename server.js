7378ebcc8980dcc65baeeac6b3abf83a1c2496ff
Edouard-Pon <91964969+Edouard-Pon@users.noreply.github.com>
2023-05-13 19:49:41 +0200

    Updated server

--- server.js
+++ server.js
@@ -6,14 +6,17 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
 const indexRouter = require('./routes/index')
 const profileRouter = require('./routes/profile')
+const projectsRouter = require('./routes/projects')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
+app.use(methodOverride('_method'))
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
@@ -24,5 +27,6 @@ mongoose.connection.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
 app.use('/profile', profileRouter)
+app.use('/projects', projectsRouter)
 
 app.listen(process.env.PORT || 3000)
